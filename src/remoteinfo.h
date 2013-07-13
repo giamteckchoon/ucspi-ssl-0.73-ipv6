d41120ff3b8b63bdeeb68ff65342a4ec8eead9ee
Giam Teck Choon <giamteckchoon@gmail.com>
2013-07-14 01:26:30 +0800

    Port Felix von Leitner's IPv6 patch for ucspi-tcp into ucspi-ssl - Part 12
    Patch: http://www.fefe.de/ucspi/ucspi-tcp-0.88-ipv6.diff19.bz2
    
    This patch just port those changes found in the patch:
    
    Patch diff of specific file name: remoteinfo.h
    Changes made in: src/remoteinfo.h
    
    Signed-off-by: Giam Teck Choon <giamteckchoon@gmail.com>

--- src/remoteinfo.h
+++ src/remoteinfo.h
@@ -5,5 +5,6 @@
 #include "uint16.h"
 
 extern int remoteinfo(stralloc *,char *,uint16,char *,uint16,unsigned int);
+extern int remoteinfo6(stralloc *,char *,uint16,char *,uint16,unsigned int,uint32);
 
 #endif
