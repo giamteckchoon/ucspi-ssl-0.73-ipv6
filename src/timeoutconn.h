898204bdc80afdbc1638b0a8e986272929044d65
Giam Teck Choon <giamteckchoon@gmail.com>
2013-07-14 01:28:47 +0800

    Port Felix von Leitner's IPv6 patch for ucspi-tcp into ucspi-ssl - Part 17
    Patch: http://www.fefe.de/ucspi/ucspi-tcp-0.88-ipv6.diff19.bz2
    
    This patch just port those changes found in the patch:
    
    Patch diff of specific file name: timeoutconn.h
    Changes made in: src/timeoutconn.h
    
    Signed-off-by: Giam Teck Choon <giamteckchoon@gmail.com>

--- src/timeoutconn.h
+++ src/timeoutconn.h
@@ -2,7 +2,9 @@
 #define TIMEOUTCONN_H
 
 #include "uint16.h"
+#include "uint32.h"
 
 extern int timeoutconn(int,char *,uint16,unsigned int);
+extern int timeoutconn6(int,char *,uint16,unsigned int,uint32);
 
 #endif
