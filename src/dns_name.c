698b55b0f444d9daf4127974c93137397b46b99f
Giam Teck Choon <giamteckchoon@gmail.com>
2013-07-14 01:21:49 +0800

    Port Felix von Leitner's IPv6 patch for ucspi-tcp into ucspi-ssl - Part 5
    Patch: http://www.fefe.de/ucspi/ucspi-tcp-0.88-ipv6.diff19.bz2
    
    This patch just port those changes found in the patch:
    
    Patch diff of specific file name: dns_name.c
    Changes made in: src/dns_name.c
    
    Signed-off-by: Giam Teck Choon <giamteckchoon@gmail.com>

--- src/dns_name.c
+++ src/dns_name.c
@@ -2,6 +2,7 @@
 #include "uint16.h"
 #include "byte.h"
 #include "dns.h"
+#include "ip6.h"
 
 static char *q = 0;
 
@@ -40,6 +41,20 @@ int dns_name4(stralloc *out,const char ip[4])
   char name[DNS_NAME4_DOMAIN];
 
   dns_name4_domain(name,ip);
+  if (dns_resolve(name,DNS_T_PTR) == -1) return -1;
+  if (dns_name_packet(out,dns_resolve_tx.packet,dns_resolve_tx.packetlen) == -1) return -1;
+  dns_transmit_free(&dns_resolve_tx);
+  dns_domain_free(&q);
+  return 0;
+}
+
+int dns_name6(stralloc *out,char ip[16])
+{
+  char name[DNS_NAME6_DOMAIN];
+
+  if (ip6_isv4mapped(ip))
+    return dns_name4(out,ip+12);
+  dns_name6_domain(name,ip);
   if (dns_resolve(name,DNS_T_PTR) == -1) return -1;
   if (dns_name_packet(out,dns_resolve_tx.packet,dns_resolve_tx.packetlen) == -1) return -1;
   dns_transmit_free(&dns_resolve_tx);
